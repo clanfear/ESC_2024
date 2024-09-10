6d3227d628b4fed9f3597861b8e365d7dca0ffea
Charles C. Lanfear <cclanfear@gmail.com>
2024-09-10 13:00:04 +0100

    Upodate

--- piza-connealy_comment/syntax/03_timeline.R
+++ piza-connealy_comment/syntax/03_timeline.R
@@ -7,17 +7,25 @@ showtext_auto()
 
 load("./piza-connealy_comment/data/derived/spd_crime_daily.RData")
 
+shootings <- tribble(~date, ~type, 
+                     "2020-06-07", "S",
+                     "2020-06-20", "S/F",
+                     "2020-06-21", "S",
+                     "2020-06-23", "S",
+                     "2020-06-29", "S/F") |>
+  mutate(date = as.Date(date))
+
 timeline <- tribble( ~date, ~event, ~position, ~ offset, ~ include,
-                     "2020-05-25", "George Floyd killed",             4 , 0, TRUE,
+                     "2020-05-25", "George Floyd killed",             14 , 0, TRUE,
                      "2020-05-29", "Downtown protests start",        0 , 0, FALSE,
                      "2020-05-30", "Downtown riots",                  0 , 0, FALSE,
-                     "2020-06-01", "Capitol Hill protests start",     4 , 8, TRUE,
+                     "2020-06-01", "Capitol Hill protests start",     14 , 8, TRUE,
                      "2020-06-05", "Tear gas ban",                    0 , 0, FALSE,
                      "2020-06-06", "SPD disperses protesters",        0 , 0, FALSE,
                      "2020-06-07", "Fernandez shooting",              0 , 0, FALSE,
                      "2020-06-07", "SPD uses tear gas, flashbangs",   0 , 4, FALSE,
-                     "2020-06-08", "Eastern precinct evacuated",      4 , 0, TRUE,
-                     "2020-07-01", "SPD returns to eastern precinct", 4 , 6,TRUE,
+                     "2020-06-08", "Eastern precinct evacuated",      14 , 0, TRUE,
+                     "2020-07-01", "SPD returns to eastern precinct", 14 , 6,TRUE,
                      "2020-07-25", "Detention center protest",        0 , 0, TRUE) |> 
   mutate(date = as.Date(date),
          y = 1 + offset,
@@ -32,14 +40,17 @@ background_plot  <-  timeline |> filter(position!=0) |>
            xmin = as.Date("2020-06-08"), 
            xmax = as.Date("2020-07-01"), 
            ymin = 0, 
-           ymax = 3, 
+           ymax = 8, 
            fill  = "black", 
            alpha = 0.3) +
-  annotate("text", label = "CHOP Period", x = as.Date("2020-06-20"), y = 2, family = "EB Garamond", color = "#b0514a", size = 10) +
+  annotate("text", label = "CHOP Period", x = as.Date("2020-06-20"), y = 5, family = "EB Garamond", color = "#b0514a", size = 10, fontface = "bold") +
   geom_segment(aes(y = 0, yend = position-0.25, xend = date), linetype = "dashed", linewidth = 0.25, color = "#333d4d") +
+  geom_segment(data = shootings, aes(y = 0, yend = 1.4, x = date, xend = date, color = type)) +
+  geom_text(data = shootings, aes(y = 2, x = date, color = type, label = type), family = "EB Garamond", size = 6) +
   geom_text(aes(y = position, label = event), vjust = 0, family = "EB Garamond", size = 10, lineheight = 0.25, color = "#333d4d") +
   scale_x_date(limits = c(as.Date("2020-05-15"), as.Date("2020-07-15")), breaks = as.Date(c("2020-05-15", "2020-06-01", "2020-06-15", "2020-07-01", "2020-07-15")), date_labels = "%B %e") +
-  scale_y_continuous(limits = c(0, 10)) +
+  scale_y_continuous(limits = c(0, 22)) +
+  scale_color_manual(values = c("S" = "#326b69", "S/F" = "#b0514a")) +
   geom_hline(yintercept = 0, color = "#333d4d") +
   theme_minimal(base_size = 32, base_family = "EB Garamond") + 
   labs(x = NULL, y = NULL) +
@@ -56,7 +67,7 @@ background_plot  <-  timeline |> filter(position!=0) |>
         axis.text.x = element_text(margin = margin(0,0,0,0, unit = "pt"), color = "#333d4d"),
         plot.margin = margin(0, 5, 5, 5, unit = "pt"))
 background_plot
-ggsave("./piza-connealy_comment/img/background_plot.png", plot = background_plot, device = ragg::agg_png, width = 16, height = 3, units = "cm")
+ggsave("./piza-connealy_comment/img/background_plot.png", plot = background_plot, device = ragg::agg_png, width = 14, height = 4, units = "cm")
 
 
 date_min <- as.Date("2020-04-01")
